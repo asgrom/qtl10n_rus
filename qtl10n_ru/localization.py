914259813fd3f75e526d6335b7c68ac9fe7c8d6d
alexandr <as.grom@gmail.com>
2020-05-29 23:36:35 +0300

    Initial commit

---
+++ qtl10n_ru/localization.py
@@ -0,0 +1,22 @@
+from PyQt5.QtCore import QTranslator
+from PyQt5.QtWidgets import QApplication
+import os
+
+
+l18n_ru = os.path.abspath('/usr/share/qt5/tranlations')
+
+
+def setupRussiaLang(app: QApplication):
+    try:
+        for file in os.listdir(l18n_ru):
+            if file.startswith('qt') and file.endswith('_ru.qm'):
+                translation = QTranslator(app)
+                translation.load(os.path.join(l18n_ru, file))
+                app.installTranslator(translation)
+    except Exception as e:
+        print('ERROR LOCALIZATION')
+        print(e)
+
+
+if __name__ == "__main__":
+    setupRussiaLang()
